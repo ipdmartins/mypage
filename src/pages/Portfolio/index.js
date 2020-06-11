41a153ae2fea78a553196858e2ef7aeebc242922
ipdmartins <ipdmartins@gmail.com>
2020-06-11 12:39:08 -0300

    developments about portfolio

--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -1,11 +1,118 @@
 import React from "react";
-
+import styled from "styled-components";
 import Layout from "../../components/Layout";
+import mypage from "../../assets/mypage.png";
+import solidity from "../../assets/solidity.png";
+
+const Container = styled.div`
+
+  h1{
+    margin: 4vh 1vw 0 1vw;
+    
+  }
+  .flex-container {
+    display: flex;
+  }
+
+  .flex-child {
+    flex: 3;
+    border: 1px solid #e0e0d1;
+    margin: 4vh 1vw 2vh 1vw;
+    height: 250px;
+    box-shadow: 1px 1px 1px 1px #ccc;
+    background-color: #f2f2f2;
+  }  
+
+  .flex-child h2{
+    margin: 1% 0 2% 2%;
+  }
+
+  .flex-child h4{
+    margin: 1% 0 2% 2%;
+  }
+
+  #imageDiv{
+    background-color: green;
+    margin: 2% 2% 0 2%;
+    width: 96%;
+    height: 70%;
+  }
+
+  #imageDiv:hover{
+    transform: scale(1.02);
+    filter: brightness(1.05);
+  }
+
+`;
 
 export default function Portfolio() {
   return (
     <Layout>
-      <h2>Portfolio</h2>
+      <Container>
+        <h1>Here you can find some codes that I have done!</h1>
+
+        <div class="flex-container">
+          <div class="flex-child magenta">
+            <div id="imageDiv">
+              <a
+                href="https://github.com/ipdmartins/mypage"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={mypage} alt="my image page" />
+              </a>
+
+            </div>
+            <h2>My webpage</h2>
+            <h4>It's built with ReactJS</h4>
+          </div>
+
+          <div class="flex-child green">
+            <div id="imageDiv">
+              <a
+                href="https://lnkd.in/dngcEsz"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={solidity} alt="my image page" />
+              </a>
+            </div>
+            <h2>My Solidity studies</h2>
+            <h4>Course with Stephen Grider</h4>
+          </div>
+
+          <div class="flex-child magenta">
+            <div id="imageDiv">
+              image
+            </div>
+            Flex Column 3
+          </div>
+        </div>
+
+        <div class="flex-container">
+          <div class="flex-child magenta">
+            <div id="imageDiv">
+              image
+            </div>
+            Flex Column 4
+          </div>
+
+          <div class="flex-child green">
+            <div id="imageDiv">
+              image
+            </div>
+            Flex Column 5
+          </div>
+
+          <div class="flex-child green">
+            <div id="imageDiv">
+              image
+            </div>
+            Flex Column 5
+          </div>
+        </div>
+
+      </Container>
     </Layout>
   );
 }
--- src/pages/Timeline/index.js
+++ src/pages/Timeline/index.js
@@ -1,10 +1,19 @@
 import React from "react";
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import { MdWork, MdSchool } from "react-icons/md";
+import { GiWorld } from "react-icons/gi";
 import Layout from "../../components/Layout";
 import styled from "styled-components";
 import bvicon from "../../assets/bvicon.png";
 import feevaleicon from "../../assets/feevaleicon.jpg";
+import santandericon from "../../assets/santandericon.png";
+import aiesecicon from "../../assets/aiesecicon.jpg";
+import carrickicon from "../../assets/carrickicon.jpg";
+import aliicon from "../../assets/aliicon.jpeg";
+import udescicon from "../../assets/udescicon.jpg";
+import vextaicon from "../../assets/vextaicon.png";
+import empdsicon from "../../assets/empdsicon.png";
+import fiergsicon from "../../assets/fiergsicon.png";
 import 'react-vertical-timeline-component/style.min.css';
 
 const Container = styled.div`
@@ -14,6 +23,9 @@ const Container = styled.div`
     }
     .title{
         margin-left:1vw;
+    }
+    .title h2, h4{
+        color: black;
     }
     #icon:hover{
         transform: scale(1.1);
@@ -28,73 +40,182 @@ export default function Timeline() {
                 <VerticalTimeline>
                     <VerticalTimelineElement
                         className="vertical-timeline-element--work"
-                        date="2011 - present"
-                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                        icon={<MdWork />}
-                    >
-                        <h3 className="vertical-timeline-element-title">Creative Director</h3>
-                        <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-                        <p>
-                            Creative Direction, User Experience, Visual Design, Project Management, Team Leading
-                    </p>
-                    </VerticalTimelineElement>
-                    <VerticalTimelineElement
-                        className="vertical-timeline-element--work"
-                        date="2010 - 2011"
-                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-
-                    >
-                        <h3 className="vertical-timeline-element-title">Art Director</h3>
-                        <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-                        <p>
-                            Creative Direction, User Experience, Visual Design, SEO, Online Marketing
-                    </p>
-                    </VerticalTimelineElement>
-                    <VerticalTimelineElement
-                        className="vertical-timeline-element--work"
-                        date="2008 - 2010"
-                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-
-                    >
-                        <h3 className="vertical-timeline-element-title">Web Designer</h3>
-                        <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
-                        <p>
-                            User Experience, Visual Design
-                    </p>
-                    </VerticalTimelineElement>
-                    <VerticalTimelineElement
-                        className="vertical-timeline-element--work"
-                        date="2006 - 2008"
-                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-
-                    >
-                        <h3 className="vertical-timeline-element-title">Web Designer</h3>
-                        <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-                        <p>
-                            User Experience, Visual Design
-                    </p>
-                    </VerticalTimelineElement>
-                    <VerticalTimelineElement
-                        className="vertical-timeline-element--education"
-                        date="April 2013"
-                        iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-
-                    >
-                        <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
-                        <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
-                        <p>
-                            Strategy, Social Media
-                    </p>
-                    </VerticalTimelineElement>
-
-                    <VerticalTimelineElement
-                        className="vertical-timeline-element--education"
-                        date="Dec/09 - sep/10"
-                        iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+                        date="Jul/19 - Curretly"
+                        icon="black"
+                        iconStyle={{ background: '#403cfc', color: '#fff' }}
                         icon={<MdWork />}
                     >
                         <div className="header">
+                            <a
+                                href="https://www.udesc.br/ceavi/empds"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <img src={empdsicon} id="icon" />
+                            </a>
 
+                            <div className="title">
+                                <h2 >Software developer intern</h2>
+                                <h4>UDESC - Escritorio Modelo, Brazil</h4>
+                            </div>
+                        </div>
+                        <p>
+                            <ul>
+                                <li>I worked in a Node.JS and React.JS project to build a management system.</li>
+                                <li>The software aims to manage Institution evaluations, patrimonial census, and public legislation.</li>
+                                <li>The purpose is to keep programming and give continuity for an application for the State Department of Education.</li>
+                            </ul>
+                        </p>
+                    </VerticalTimelineElement>
+
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        date="Jul/17 - Sep/17"
+                        iconStyle={{ background: '#403cfc', color: '#fff' }}
+                        icon={<MdWork />}
+                    >
+                        <div className="header">
+                            <a
+                                href="https://www.vexta.com.br/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <img src={vextaicon} id="icon" />
+                            </a>
+
+                            <div className="title">
+                                <h2>Technical support</h2>
+                                <h4>Vexta Systems, Brazil</h4>
+                            </div>
+                        </div>
+                        <p>I was responsible to manage the first contact with the user, aiming to solve clients' needs related to the system.</p>
+                    </VerticalTimelineElement>
+
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--education"
+                        date="Feb/17 - currently"
+                        iconStyle={{ background: '#403cfc', color: '#fff' }}
+                        icon={<MdSchool />}
+                    >
+                        <div className="header">
+                            <a
+                                href="https://www.udesc.br/international"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <img src={udescicon} id="icon" />
+                            </a>
+                            <div className="title">
+                                <h2>Bachelor in Software Engineering</h2>
+                                <h4>UDESC (State University), Brazil</h4>
+                            </div>
+                        </div>
+                    </VerticalTimelineElement>
+
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        date="Apr/15 - Sep/16"
+                        iconStyle={{ background: '#403cfc', color: '#fff' }}
+                        icon={<MdWork />}
+                    >
+                        <div className="header">
+                            <a
+                                href="https://www.sesirs.org.br/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <img src={fiergsicon} id="icon" />
+                            </a>
+                            <div className="title">
+                                <h2>English instructor</h2>
+                                <h4>SESI RS, Brazil</h4>
+                            </div>
+                        </div>
+                        <p>I conducted general English classes at school and conversation groups in companies.</p>
+                    </VerticalTimelineElement>
+
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        date="Sep/12 - Oct/14"
+                        iconStyle={{ background: '#403cfc', color: '#fff' }}
+                        icon={<MdWork />}
+                    >
+                        <div className="header">
+                            <a
+                                href="shorturl.at/dyKO1"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <img src={aliicon} id="icon" />
+                            </a>
+                            <div className="title">
+                                <h2>Innovation and management advisor</h2>
+                                <h4>SEBRAE - ALI's Project , Brazil</h4>
+                            </div>
+                        </div>
+                        <p>
+                            <ul>
+                                <li>I created group of 55 small business from commerce, industry, and services.</li>
+                                <li>I diagnosed all the companies to establish an action plan, aiming innovation and management improvements.</li>
+                                <li>I accompanied the businesses for two years to help in the implementation plan.</li>
+                                <li>I measured the companies according to a scientific method, and two articles were published by CNPq Brazil.</li>
+                            </ul>
+                        </p>
+                    </VerticalTimelineElement>
+
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--education"
+                        date="Apr/10 - Apr/12"
+                        iconStyle={{ background: '#403cfc', color: '#fff' }}
+                        icon={<GiWorld />}
+                    >
+                        <div className="header">
+                            <a
+                                href="https://www.linkedin.com/company/carrick-institute-of-education/about/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <img src={carrickicon} id="icon" />
+                            </a>
+                            <div className="title">
+                                <h2>Exchange for English studies</h2>
+                                <h4>Sydney/Australia</h4>
+                            </div>
+                        </div>
+                    </VerticalTimelineElement>
+
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--education"
+                        date="Oct/10 - Feb/11"
+                        iconStyle={{ background: '#403cfc', color: '#fff' }}
+                        icon={<GiWorld />}
+                    >
+                        <div className="header">
+                            <a
+                                href="https://aiesec.org/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <img src={aiesecicon} id="icon" />
+                            </a>
+                            <div className="title">
+                                <h2>International exchange as volunteer</h2>
+                                <h4>AIESEC Cameroun/Africa</h4>
+                            </div>
+                        </div>
+                        <p>
+                            I was part of a project in a local school where the <br />
+                        objective was to promote environmental awareness
+                        </p>
+                    </VerticalTimelineElement>
+
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        date="Dec/09 - sep/10"
+                        iconStyle={{ background: '#403cfc', color: '#fff' }}
+                        icon={<MdWork />}
+                    >
+                        <div className="header">
                             <a
                                 href="https://www.bv.com.br/"
                                 target="_blank"
@@ -102,12 +223,9 @@ export default function Timeline() {
                             >
                                 <img src={bvicon} id="icon" />
                             </a>
-
                             <div className="title">
-                                <h2 className="vertical-timeline-element-title">Commercial credit operator</h2>
-                                <h4 className="vertical-timeline-element-subtitle">
-                                    BV Financeira, Brazil
-                            </h4>
+                                <h2>Commercial credit operator</h2>
+                                <h4>BV Financeira, Brazil</h4>
                             </div>
                         </div>
                         <p>
@@ -120,9 +238,62 @@ export default function Timeline() {
                     </VerticalTimelineElement>
 
                     <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        date="Jul/07 - Mar/09"
+                        iconStyle={{ background: '#403cfc', color: '#fff' }}
+                        icon={<MdWork />}
+                    >
+                        <div className="header">
+                            <a
+                                href="https://www.santander.com.br/hotsite/santanderfinanciamentos/index.html"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <img src={santandericon} id="icon" />
+                            </a>
+                            <div className="title">
+                                <h2>Administrative assistant</h2>
+                                <h4>ABN AMRO BANK (Aymore Financiamentos), Brazil</h4>
+                            </div>
+                        </div>
+                        <p>
+                            <ul>
+                                <li>Responsible for supporting commercial team needs</li>
+                                <li>Management of administrative tasks related to vehicle financing</li>
+                                <li>Credit analysis of clients for financing and leasing.</li>
+                            </ul>
+                        </p>
+                    </VerticalTimelineElement>
+
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--education"
+                        date="Sep/05 - Sep/07"
+                        iconStyle={{ background: '#403cfc', color: '#fff' }}
+                        icon={<MdSchool />}
+                    >
+                        <div className="header">
+                            <a
+                                href="https://aiesec.org/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <img src={aiesecicon} id="icon" />
+                            </a>
+                            <div className="title">
+                                <h2>Volunteer operational director</h2>
+                                <h4>AIESEC, Brazil</h4>
+                            </div>
+                        </div>
+                        <p>
+                            I promoted exchanges of academic students,
+                            organized events, and managed a team.
+                        </p>
+                    </VerticalTimelineElement>
+
+                    <VerticalTimelineElement
                         className="vertical-timeline-element--education"
                         date="Feb/05 - dec/09"
-                        iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+                        iconStyle={{ background: '#403cfc', color: '#fff' }}
                         icon={<MdSchool />}
                     >
                         <div className="header">
@@ -134,8 +305,8 @@ export default function Timeline() {
                                 <img src={feevaleicon} id="icon" />
                             </a>
                             <div className="title">
-                                <h3 className="vertical-timeline-element-title">Bachelor in Business Administration</h3>
-                                <h4 className="vertical-timeline-element-subtitle">Feevale University, Brazil</h4>
+                                <h2>Bachelor in Business Administration</h2>
+                                <h4>Feevale University, Brazil</h4>
                             </div>
                         </div>
                     </VerticalTimelineElement>
